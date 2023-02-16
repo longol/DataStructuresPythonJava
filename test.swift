2a02424e6a24b1efcb2d9b5dc338d3dec3c6c208
Lucas Longo <lucaslongo@gmail.com>
2023-02-16 16:29:22 +0000

    swift files added

---
+++ test.swift
@@ -0,0 +1,14 @@
+
+var pessoasNoCinema = [
+    ["João", "Maria", "Luiz"],
+    ["Amanda", "José", "Paulo"]
+]
+
+var segundaFileira = pessoasNoCinema[1]
+var segundaFileiraTerceiraPessoa = segundaFileira[2]
+
+var combo1 = pessoasNoCinema[1][2]
+
+pessoasNoCinema[0][1] = "Mario"
+
+print(pessoasNoCinema[0])
